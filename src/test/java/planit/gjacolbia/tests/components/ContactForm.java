a425ad32fb6eb9c119fdfafec08b763884ccb903
gerardjacolbia <gerard.jacolbia@gmail.com>
2021-07-05 13:04:12 +0800

    Refactor to use a more generic approach in getting webelements via reflection.

--- src/test/java/planit/gjacolbia/tests/components/ContactForm.java
+++ src/test/java/planit/gjacolbia/tests/components/ContactForm.java
@@ -13,7 +13,6 @@ import org.testng.Assert;
 import planit.gjacolbia.framework.Configuration;
 
 import java.util.Arrays;
-import java.util.Optional;
 
 @Log4j2
 public class ContactForm extends LoadableComponent<ContactForm> {
@@ -23,6 +22,18 @@ public class ContactForm extends LoadableComponent<ContactForm> {
         EMAIL,
         TELEPHONE,
         MESSAGE
+    }
+
+    enum FieldType {
+        CONTROL_GROUP("ControlGroup"),
+        FIELD("Field"),
+        ERROR_MESSAGE("ErrorMessage");
+
+        String text;
+
+        FieldType(String textValue) {
+            text = textValue;
+        }
     }
 
     private static final By PAGE_LOADED_INDICATOR = By.xpath("//a[contains(@class, 'btn-contact') and contains(@ui-event,'onSubmit') and text()='Submit']");
@@ -76,51 +87,23 @@ public class ContactForm extends LoadableComponent<ContactForm> {
     }
 
     public boolean isFieldInvalid(Field field) {
-        // TODO have a field getter, then throw if not found
-        var classFields = ContactForm.class.getDeclaredFields();
-        Optional<WebElement> controlGroupElement = Arrays.stream(classFields).filter(classField -> isControlGroupElement(classField, field)).map(this::mapToWebElement).findFirst();
-        Optional<WebElement> formFieldElement = Arrays.stream(classFields).filter(classField -> isFormFieldElement(classField, field)).map(this::mapToWebElement).findFirst();
-
-        if (controlGroupElement.isPresent() && formFieldElement.isPresent()) {
-            return controlGroupElement.get().getAttribute("class").contains("error") && formFieldElement.get().getAttribute("class").contains("ng-invalid");
-        } else {
-            log.error(field + " does not have a ControlGroup element or a FieldElement in the page object.");
-        }
-
-        return false;
+        var controlGroupElement = getWebElementReflection(field, FieldType.CONTROL_GROUP);
+        var formFieldElement = getWebElementReflection(field, FieldType.FIELD);
+        return controlGroupElement.getAttribute("class").contains("error") && formFieldElement.getAttribute("class").contains("ng-invalid");
     }
 
     public String getFieldErrorMessage(Field field) {
         if (isFieldInvalid(field)) {
-            // TODO have a field getter, then throw if not found
-            var classFields = ContactForm.class.getDeclaredFields();
-            Optional<WebElement> errorMessageElement = Arrays.stream(classFields).filter(classField -> isFormErrorElement(classField, field)).map(this::mapToWebElement).findFirst();
-            if (errorMessageElement.isPresent()) {
-                return errorMessageElement.get().getText();
-            } else {
-                log.error(field + " does not have a FieldElement in the page object.");
-            }
+            WebElement errorMessageElement = getWebElementReflection(field, FieldType.ERROR_MESSAGE);
+            return errorMessageElement.getText();
         }
         return "";
     }
 
-    private boolean isControlGroupElement(java.lang.reflect.Field classField, Field formField) {
-        return isWebElementOfPrefixSuffix(classField, formField.name().toLowerCase(), "ControlGroup");
-    }
-
-    private boolean isFormFieldElement(java.lang.reflect.Field classField, Field formField) {
-        return isWebElementOfPrefixSuffix(classField, formField.name().toLowerCase(), "Field");
-    }
-
-    private boolean isFormErrorElement(java.lang.reflect.Field classField, Field formField) {
-        return isWebElementOfPrefixSuffix(classField, formField.name().toLowerCase(), "ErrorMessage");
-    }
-
-    private boolean isWebElementOfPrefixSuffix(java.lang.reflect.Field classField, String prefix, String suffix) {
-        String classFieldName = classField.getName();
-        return classField.getType().equals(WebElement.class)
-                && classFieldName.startsWith(prefix)
-                && classFieldName.endsWith(suffix);
+    private WebElement getWebElementReflection(Field formField, FieldType formFieldType) {
+        var classFields = ContactForm.class.getDeclaredFields();
+        String webElementName = formField.name().toLowerCase() + formFieldType.text;
+        return Arrays.stream(classFields).filter(classField -> classField.getName().equals(webElementName)).map(this::mapToWebElement).findFirst().orElseThrow(() -> new Error("WebElement field with name " + webElementName + " not found in class."));
     }
 
     private WebElement mapToWebElement(java.lang.reflect.Field classField) {
@@ -132,15 +115,10 @@ public class ContactForm extends LoadableComponent<ContactForm> {
     }
 
     public void fill(Field field, String text) {
-        var classFields = ContactForm.class.getDeclaredFields();
-        Optional<WebElement> formFieldElement = Arrays.stream(classFields).filter(classField -> isFormFieldElement(classField, field)).map(this::mapToWebElement).findFirst();
-        if (formFieldElement.isPresent()) {
-            formFieldElement.get().click();
-            formFieldElement.get().clear();
-            formFieldElement.get().sendKeys(text);
-        } else {
-            log.error(field + " does not have a FieldElement in the page object.");
-        }
+        WebElement formFieldElement = getWebElementReflection(field, FieldType.FIELD);
+        formFieldElement.click();
+        formFieldElement.clear();
+        formFieldElement.sendKeys(text);
     }
 
     public void clickSubmit() {
